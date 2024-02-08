ecc870a029cca40b016e247bcd0a59274cf16a34
punitkr03 <tagpunitkumar@gmail.com>
2024-02-08 20:15:36 +0530

    cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b.d
@@ -0,0 +1,5 @@
+/home/punit/codes/rustyy/hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b: src/main.rs
+
+/home/punit/codes/rustyy/hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/punit/codes/rustyy/hello_cargo/target/debug/hello_cargo: /home/punit/codes/rustyy/hello_cargo/src/main.rs
